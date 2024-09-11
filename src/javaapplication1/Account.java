6d6745f04c46cc848065c23f895e7ea2aca5cc58
Lucelmae <jldakaysedut@gmail.com>
2024-09-11 12:09:11 +0800

    my update class

--- src/javaapplication1/Account.java
+++ src/javaapplication1/Account.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Account {
     
-    
-     public void Account(){
-    
+    public void Account() {
         Scanner sc = new Scanner(System.in);
         Accounts[] ar = new Accounts[100];
 
@@ -27,22 +25,34 @@ public class Account {
             String email = sc.nextLine();
             System.out.print("Username: ");
             String username = sc.nextLine();
-            System.out.print("Password: ");
-            String password = sc.nextLine();
+
+           
+            String password;
+            while (true) {
+                System.out.print("Password: ");
+                password = sc.nextLine();
+
+                if (password.length() < 8) {
+                    System.out.println("Password is too short, must be at least 8 characters. Try again.");
+                } else if (!password.matches(".*[a-zA-Z].*") || !password.matches(".*[0-9].*")) {
+                    System.out.println("Password must contain both letters and numbers. Try again.");
+                } else {
+                    break; 
+                }
+            }
 
             ar[i] = new Accounts();
             ar[i].addAccounts(id, firstName, lastName, email, username, password);
         }
-        
-        
+
+       
         System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s \n",
-        "ID", "NAME", "LASTNAME", "EMAIL", "USERNAME", "PASSWORD");
+                "ID", "NAME", "LASTNAME", "EMAIL", "USERNAME", "PASSWORD");
 
         for (int i = 0; i < nums; i++) {
             ar[i].viewAccounts();
         }
 
         sc.close();
-    
-}
+    }
 }
--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -4,7 +4,7 @@ package javaapplication1;
 
 public class JavaApplication1 {
 
-    private static String ad;
+    
     
 
  
@@ -12,27 +12,28 @@ public class JavaApplication1 {
         
         
         
-        /*  System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
+        /*       System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
         "ID", "Name", "Sold", "Stock", "Price", "Profit", "Status", "Expected Profit");
         Products pr = new Products();
-        pr.addProducts(1000, "CHIHARON", 20, 30, 20.00);
+        pr.addProducts(1000, "CHIHARON", 30, 0, 20.00);
         pr.viewProducts();*/
         
-        /*Product pr = new Product();
-        pr.Product();
-        */
+        
+        /*  Product pr = new Product();
+        pr.Product();*/
         
         
         
         
         
-        /*  System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s \n",
+        
+        /*         System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s \n",
         "ID", "NAME", "PRELIM", "MIDTERM", "PREFINAL", "FINAL", "AVERAGE", "REMARKS");
         Grades gr = new  Grades();
         gr.addGrades(500, "loydi", 2.5, 3.5, 3.1, 3.1);
         gr.viewGrades(); */
-            
-        /*  Grade gr = new Grade();
+           
+        /* Grade gr = new Grade();
         gr.Grade();
         */
 
@@ -40,16 +41,18 @@ public class JavaApplication1 {
         
         
         
-        
-        /*   Salary sr = new Salary();
+        /*
+        Salary sr = new Salary();
         sr.Salary();
         */
         
-        /*System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s \n",
+        
+        /*        System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s \n",
         "ID", "NAME", "RATE", "HOURS", "GROSS", "DEDUCT", "NETPAY");
         Salaries sr = new Salaries();
         sr.addSalaries(100, "john", 600, 50, 1000);
         sr.viewSalaries();*/
+      
         
         
         
@@ -57,19 +60,19 @@ public class JavaApplication1 {
         
         
         
+        /*
         
-        
-        /* System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s \n",
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s \n",
         "ID", "NAME", "LASTNAME", "EMAIL", "USERNAME", "PASSWORD");
         Accounts sr = new Accounts();
         sr.addAccounts(1, "john", "dakay", "@gmail", "sedut", "1234ee");
         sr.viewAccounts();*/
         
-        /*
-        Account ar = new Account();
-        ar.Account();*/
         
+        /*         Account ar = new Account();
+        ar.Account();
         
+        */
    
 
         
